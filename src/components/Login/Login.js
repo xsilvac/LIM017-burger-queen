2a75427ae32d3b09fdcc82a06069f432bcfd0e67
JossFranco <jossfranco70@gmail.com>
2022-06-10 20:54:09 -0500

    Añadir imagen al login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Login = () => {
   return (
     <div>
+      <img src={require("../../img/burger.png")}  alt="fondo" />
         <h2>Hola, al login</h2>
     </div>
   )
