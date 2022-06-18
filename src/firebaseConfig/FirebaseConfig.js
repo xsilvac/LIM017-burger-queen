8e34a071ff2a73cd044a94e2878bb39cb6903de9
JossFranco <jossfranco70@gmail.com>
2022-06-17 22:16:53 -0500

    vista del admi 50/100

--- src/firebaseConfig/FirebaseConfig.js
+++ src/firebaseConfig/FirebaseConfig.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
@@ -14,5 +16,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
- 
+export const db = getFirestore(app);
+export const storage = getStorage(app);
