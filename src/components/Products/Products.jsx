4ae259a24621943cda7a4fbbf663023e3f8b24a7
ximena silva <ximenasilvacotrina11@gmail.com>
2022-06-24 17:41:03 -0500

    Crear componentes para la vistas de breakfast y lunch. Close #44

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,38 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebaseConfig/FirebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc, deleteDoc } from "firebase/firestore";
 import './Products.css';
 import Navbar from "../Navbar/Navbar"
+import { GrTrash } from "react-icons/gr";
+import {useNavigate} from "react-router-dom";
+
+
 
 const Products = () => {
-const [list, setList] = useState([]);
+    const [products, setProducts] = useState([])
+    console.log('holaa1 estamos iniciando');
+    const productsLunch = collection(db, 'Lunch')
+    const productsBreakfast = collection(db, 'Breakfast')
+    console.log('holaaa2 estamos en lunch');
 
-useEffect(() => {
- const getList = async() => {
-    try {
-        const querySnapshot = await getDocs(collection(db, "Lunch"))
-        const docs = []
-        querySnapshot.forEach((doc) => {
-            docs.push({...doc.data(), id:doc.id})
-        })
-        setList(docs)
-    } catch (err) {
-        console.log(err.message)
+    const navigate = useNavigate();
+
+    const getLunch = async () => {
+    const data = await getDocs(productsLunch)
+    console.log('holaaa3 estamos iniciando getdocs')
+    setProducts(
+        data.docs.map((doc) => ({...doc.data(),id:doc.id})),
+        console.log(data.docs.map((doc) => ({...doc.data(),id:doc.id})))
+    )
+    console.log('probando')
+    };
+    
+    const deleteProduct = async (id) => {
+        const productDoc = doc(db, 'Lunch', id)
+        await deleteDoc(productDoc)
+        getLunch()
     }
- }
- getList();
-}, [list])
-
+    useEffect(() => {
+        getLunch()
+        console.log('holaa4 estamos en use effect')
+    }, [] )
   return (
     <><Navbar />
     <div className='containerProductsFirst'>
         <div className='containerProducts'>
-            {
-                list.map(list => (
+            <button onClick={() => navigate("/Breakfast")}>Desayuno</button>            {
+                products.map(list => (
                     <div key={list.id} className='containerCard'>
                         <p className='productName'>{list.ProductName}</p>
                         <p className='productPrice'>Precio: ${list.ProductPrice}</p>
                         <img className='productImg'src={list.ProductImg} alt='' />
+                        <button onClick={() => {deleteProduct(list.id)}} className='btnDelete'><GrTrash /></button>
                         <p></p>
                     </div>
                 ))
