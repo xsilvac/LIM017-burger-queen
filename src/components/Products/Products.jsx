b181b886b56e7bc13e8b0babec145287470d6caa
JossFranco <jossfranco70@gmail.com>
2022-06-23 20:41:59 -0500

    products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,38 +1,50 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../firebaseConfig/FirebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc, deleteDoc } from "firebase/firestore";
 import './Products.css';
 import Navbar from "../Navbar/Navbar"
+import { GrTrash } from "react-icons/gr";
+
 
 const Products = () => {
-const [list, setList] = useState([]);
+    const [products, setProducts] = useState([])
+    console.log('holaa1 estamos iniciando');
+    const productsCollection = collection(db, 'Lunch')
+    console.log('holaaa2 estamos en lunch');
 
-useEffect(() => {
- const getList = async() => {
-    try {
-        const querySnapshot = await getDocs(collection(db, "Lunch"))
-        const docs = []
-        querySnapshot.forEach((doc) => {
-            docs.push({...doc.data(), id:doc.id})
-        })
-        setList(docs)
-    } catch (err) {
-        console.log(err.message)
+    const getProducts = async () => {
+    const data = await getDocs(productsCollection)
+    console.log('holaaa3 estamos iniciando getdocs') 
+    setProducts(
+        data.docs.map((doc) => ({...doc.data(),id:doc.id})),
+        console.log('holaa4 estamos en setproducts')
+    )
+    console.log(data.data)   
+    };
+
+    const deleteProduct = async (id) => {
+        const productDoc = doc(db, 'products', id)
+        await deleteDoc(productDoc)
+        getProducts()
     }
- }
- getList();
-}, [list])
+
+
+    useEffect(() => {
+        getProducts()
+        console.log('holaa4 estamos engetproducts')
+    },[])
 
   return (
     <><Navbar />
     <div className='containerProductsFirst'>
         <div className='containerProducts'>
             {
-                list.map(list => (
+                products.map(list => (
                     <div key={list.id} className='containerCard'>
                         <p className='productName'>{list.ProductName}</p>
                         <p className='productPrice'>Precio: ${list.ProductPrice}</p>
                         <img className='productImg'src={list.ProductImg} alt='' />
+                        <button onClick={() => {deleteProduct(list.id)}} className='btnDelete'><GrTrash /></button>
                         <p></p>
                     </div>
                 ))
