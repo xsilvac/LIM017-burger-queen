a8ed125cb5de9c8dd8b877286bf175b2fd8a4660
ximena silva <ximenasilvacotrina11@gmail.com>
2022-07-04 12:04:36 -0500

    Agregar productos a la lista de pedidos. Close #58

--- src/page/Waiter/MenuItem.jsx
+++ src/page/Waiter/MenuItem.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from "react";
+import React/* , { useState } */ from "react";
 
-function MenuItem({item, onchangeAmount}) {
-    const [amount, setAmount] = useState(0)
+function MenuItem({item/* , amount, setAmount */, changeAmount}) {
+   /*  const [amount, setAmount] = useState(0) */
     const increase = () => {
-        setAmount(amount + 1) ;
-        onchangeAmount()
+        //setAmount(amount + 1) ;
+        changeAmount(item.id, "increase");
     }
     const decrease = () => {
-        if(amount>=1){
-        setAmount(amount - 1);
-        onchangeAmount()
-    }
-    } 
+        //setAmount(amount - 1);
+        changeAmount(item.id,"decrease");
+}
     return (
         <div key={item.id} className="col w-40">
             <div className="card h-80 w-30">
@@ -21,7 +19,7 @@ function MenuItem({item, onchangeAmount}) {
                     <p className="card-text fs-6">${item.ProductPrice}</p>
                     <div className="row row-cols-3">
                         <button className="btn btn-success" onClick={increase}>+</button>
-                        <p className="card-text fs-5">{amount}</p>
+                        {/* <p className="card-text fs-5">{item.amount}</p> */}
                         <button className="btn btn-danger" onClick={decrease}>-</button>
                     </div>
                 </div>
