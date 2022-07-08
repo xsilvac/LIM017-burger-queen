941733216b880d5c04fd079b879b75da03a33082
JossFranco <jossfranco70@gmail.com>
2022-07-07 19:03:14 -0500

    Test Login 70%

--- src/page/Login/Login.test.js
+++ src/page/Login/Login.test.js
@@ -1,46 +1,85 @@
-import React from 'react';
- import Login from './Login.jsx';
-import {fireEvent, render, screen, waitFor} from '@testing-library/react';
-import '@testing-library/jest-dom';
-import {createMemoryHistory} from 'history';
-import {Router} from 'react-router-dom';
-import {signInWithEmailAndPassword} from '../../firebaseConfig/FirebaseConfig';
+import React from "react";
+import Login from "./Login.jsx";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { createMemoryHistory } from "history";
+import { Router } from "react-router-dom";
+import { signInWithEmailAndPassword } from "../../firebaseConfig/FirebaseConfig";
 
-jest.mock('../../firebaseConfig/FirebaseConfig.jsx')
+jest.mock("../../firebaseConfig/FirebaseConfig.jsx");
 
-describe('Renderizar Login', () => {
-  it('Render Login', () => {
+describe("Renderizar Login", () => {
+  it("Render Login", () => {
     const history = createMemoryHistory();
     render(
       <Router location={history.location} navigator={history}>
-        <Login/>
-        </Router>
-    )
-    const btnLogin = screen.getByTestId('btnLogin');
+        <Login />
+      </Router>
+    );
+    const btnLogin = screen.getByTestId("btnLogin");
     expect(btnLogin).toBeInTheDocument();
   });
 });
 
-describe('Componente Login',()=>{
-it('Componente login', async () => {
+describe("Componente Login", () => {
+  it("Iniciar sesión de managger", async () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>
+    );
+    const emailImput = screen.getByPlaceholderText("Email");
+    const pswInput = screen.getByPlaceholderText("contraseña");
+    const valueSelect = screen.getByTestId("selectLogin");
+    fireEvent.change(emailImput, { target: { value: "jossel78@hotmail.com" } });
+    fireEvent.change(pswInput, { target: { value: "1234567" } });
+    fireEvent.change(valueSelect, { target: { value: "managger" } });
+    const btnLogin = screen.getByText("INGRESAR");
 
-  const history = createMemoryHistory()
-        render(
-          <Router location={history.location} navigator={history}>
-            <Login />
-          </Router>,
-        );
-        const emailImput = screen.getByPlaceholderText('Email')
-        const pswInput = screen.getByPlaceholderText('contraseña')
-        const valueSelect = screen.getByTestId('selectLogin');
-        fireEvent.change(emailImput, {target: {value: 'jossel78@hotmail.com'}})
-        fireEvent.change(pswInput,{target: {value: '1234567'}})
-        fireEvent.change(valueSelect, {target: {value:'managger'}})
-        const btnLogin = screen.getByText('INGRESAR')
+    fireEvent.click(btnLogin);
+    await waitFor(() => {
+      expect(history.location.pathname).toBe("/Lunch");
+    });
+  });
 
-        fireEvent.click(btnLogin)
-await waitFor(() =>{
-expect(history.location.pathname).toBe('/Lunch')
-})
-      });
+  it("Iniciar sesión de chef", async () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>
+    );
+    const emailImput = screen.getByPlaceholderText("Email");
+    const pswInput = screen.getByPlaceholderText("contraseña");
+    const valueSelect = screen.getByTestId("selectLogin");
+    fireEvent.change(emailImput, { target: { value: "jossel78@hotmail.com" } });
+    fireEvent.change(pswInput, { target: { value: "1234567" } });
+    fireEvent.change(valueSelect, { target: { value: "chef" } });
+    const btnLogin = screen.getByText("INGRESAR");
+    fireEvent.click(btnLogin);
+    await waitFor(() => {
+      expect(history.location.pathname).toBe("/Kitchen");
+    });
+  });
+
+  it("Iniciar sesión de waiter", async () => {
+    const history = createMemoryHistory();
+    render(
+      <Router location={history.location} navigator={history}>
+        <Login />
+      </Router>
+    );
+    const emailImput = screen.getByPlaceholderText("Email");
+    const pswInput = screen.getByPlaceholderText("contraseña");
+    const valueSelect = screen.getByTestId("selectLogin");
+    fireEvent.change(emailImput, { target: { value: "jossel78@hotmail.com" } });
+    fireEvent.change(pswInput, { target: { value: "1234567" } });
+    fireEvent.change(valueSelect, { target: { value: "waiter" } });
+    const btnLogin = screen.getByText("INGRESAR");
+    fireEvent.click(btnLogin);
+    await waitFor(() => {
+      expect(history.location.pathname).toBe("/WaiterLunch");
+    });
+  });
 });
